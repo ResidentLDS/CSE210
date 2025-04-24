a1916e096ae8539ac1b5852a81bbcd4624af1d77
ResidentLDS <residentlds6587@gmail.com>
2025-04-24 13:00:03 -0600

    git

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.ComponentModel.Design.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //Time to become spies...
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
