39b8f9e97621bf18d8d73a635f809f737d5796de
ResidentLDS <residentlds6587@gmail.com>
2025-05-03 15:27:15 -0600

    Wrote code for number list

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,25 +5,27 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        string again = "yes";
         Random rnd = new Random();
-        while (again == 'yes'|| again == 'Yes')
-        int guess = 0;
-        int magic = rnd.Next(1,11);
-        while (guess != magic)
+        while (again == "yes"|| again == "Yes")
         {
-            Console.WriteLine("What is your guess? ");
-            string input = Console.ReadLine();
-            guess = int.Parse(input);
-            if (guess > magic)
+            int guess = 0;
+            int magic = rnd.Next(1,11);
+            while (guess != magic)
             {
-                Console.WriteLine("Lower");
+                Console.WriteLine("What is your guess? ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
             }
-            else if (guess < magic)
-            {
-                Console.WriteLine("Higher");
-            }
+            Console.WriteLine($"You win! You guessed: {magic}");
         }
-        Console.WriteLine($"You win! You guessed: {magic}");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int enter = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (enter != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            string input = Console.ReadLine();
+            enter = int.Parse(input);
+            if (enter != 0)
+            {
+                numbers.Add(enter);
+            }
+        }
+        int sum = 0;
+        int large = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > large)
+            {
+                large = number;
+            }
+        }
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"Sum: {sum} \nAverage: {average}\nLargest number: {large}");
+        
+
     }
 }
